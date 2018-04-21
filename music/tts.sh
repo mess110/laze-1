01d62ebdb90f479f1505ae900926a3176da84bb5
Cristian Mircea Messel <mess110@gmail.com>
2018-04-21 19:54:07 +0300

    add volume to tts

--- music/tts.sh
+++ music/tts.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-say() { /usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
+say() { /usr/bin/mplayer -ao alsa -volume 100 -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
+# say() { /usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
 say $*

499c80bbb6444c7950638cc00a98fbbf499ba917
Cristian Mircea Messel <mess110@gmail.com>
2018-04-21 20:05:21 +0300

    use exec

--- music/app.js
+++ music/app.js
@@ -1,9 +1,27 @@
 const http = require('http')
+const url = require('url')
+const exec = require('child_process').exec
+
 const port = 3000
 
 const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
+  const url_parts = url.parse(request.url, true)
+  const query = url_parts.query
+  if (query.cmd === undefined || query.cmd === null) {
+    response.end('missing cmd param')
+    return
+  }
+  const cmd = query.cmd
+
+  const child = exec(cmd, function (error, stdout, stderr) {
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    if (error !== null) {
+      console.log(`exec error: ${error}`);
+    }
+  });
+
+  response.end(cmd)
 }
 
 const server = http.createServer(requestHandler)
